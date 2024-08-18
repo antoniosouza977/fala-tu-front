25402c4537c5d29b2095df48ee9f041d07eebf5c
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2024-08-17 23:17:33 -0400

    fix: componentes de validação

---
+++ src/components/InputValidationErrors.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-if="errors">
+        <p v-for="error in errors" class="text-red-400 font-bold mt-3">{{ error }}</p>
+      </div>
+</template>
+
+<script>
+
+export default {
+    props: ['errors']
+}
+
+</script>
