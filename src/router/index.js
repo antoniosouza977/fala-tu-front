eeaa185d1be6cc1ec79494e974ca13345add368d
Antonio Souza <129562096+antoniosouza977@users.noreply.github.com>
2024-08-17 23:20:09 -0400

    feat: ajustes rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import LoginView from "@/views/LoginView.vue";
-import AboutView from "@/views/AboutView.vue";
+import LoginView from "@/views/auth/LoginView.vue";
+import RegisterView from '@/views/auth/RegisterView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,17 +9,26 @@ const router = createRouter({
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: HomeView,
+            meta: {
+                requiresAuth: true
+            }
         },
         {
             path: '/login',
             name: 'login',
-            component: LoginView
+            component: LoginView,
+            meta: {
+                requiresAuth: false
+            }
         },
         {
-            path: '/about',
-            name: 'about',
-            component: AboutView
+            path: '/register',
+            name: 'register',
+            component: RegisterView,
+            meta: {
+                requiresAuth: false
+            }
         }
     ]
 })
